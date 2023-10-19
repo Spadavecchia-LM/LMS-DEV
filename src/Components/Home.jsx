ebe3a148ad09e305509ca1131cb8ff9c28f57e4a
spadavecchia-lm <leonardo.spadavecchia@hotmail.com>
2023-10-19 11:48:15 -0300

    primer commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button, ButtonGroup} from "@nextui-org/react";
+
+const Home = () => {
+  return (
+    <>
+    <Button color='secondary' variant='ghost'>LMS DEV</Button>
+    </>
+  )
+}
+
+export default Home
